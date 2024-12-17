c919dbb086d9eaf48a0d97f02e14dba72ba17587
StimoloRiccardo <rikisti007@gmail.com>
2024-12-17 09:28:28 +0100

    creazione progetto c++

---
+++ blackjack_natalizio/blackjack_natalizio/main.cpp
@@ -0,0 +1,14 @@
+#define IMM2D_WIDTH 640
+#define IMM2D_HEIGHT 480
+#define IMM2D_SCALE 1
+
+#define IMM2D_IMPLEMENTATION
+#include "immediate2d.h"
+#include <string>
+
+using namespace std;
+
+void run() {
+
+
+}
