cd65c1af95682d1c1a75ce6085f1c5c67bc31c14
thomascgl <thomas.cgl27@gmail.com>
2024-12-17 09:57:11 +0100

    crezione tavolo prova

--- blackjack_natalizio/blackjack_natalizio/main.cpp
+++ blackjack_natalizio/blackjack_natalizio/main.cpp
@@ -1,5 +1,5 @@
-#define IMM2D_WIDTH 640
-#define IMM2D_HEIGHT 480
+#define IMM2D_WIDTH 1920
+#define IMM2D_HEIGHT 1080
 #define IMM2D_SCALE 1
 
 #define IMM2D_IMPLEMENTATION
@@ -7,8 +7,20 @@
 #include <string>
 
 using namespace std;
-
+int colore = MakeColor(0, 49, 83);
+int colortav = MakeColor(139, 69, 19);
+int coloretav2 = MakeColor(0, 100, 0);
 void run() {
+    int colore = MakeColor(0, 49, 83);
+    int colortav = MakeColor(139, 69, 19);
+    int coloretav2 = MakeColor(0, 100, 0);
+    
+    
+        UseAntiAliasing();
+        Clear(colore);
+        DrawCircle(960, 0, 900, colortav, colortav);
+        DrawCircle(960, 0, 800, coloretav2, coloretav2);
 
+    
 
 }
