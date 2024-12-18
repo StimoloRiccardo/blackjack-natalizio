8c6bf76d51a9ef42d374bc33266839e7fc0548fe
thomascgl <thomas.cgl27@gmail.com>
2024-12-18 20:32:20 +0100

    random seme,numero carte (capire come non far ripetere alcune carte,capire le regole del gioco)

--- blackjack_natalizio/blackjack_natalizio/main.cpp
+++ blackjack_natalizio/blackjack_natalizio/main.cpp
@@ -5,10 +5,9 @@
 #define IMM2D_IMPLEMENTATION
 #include "immediate2d.h"
 #include <string>
-#include <time.h>
-#include <cstdlib>
 using namespace std;
 void carta(int x,int y);
+int numero[7]; 
 int colore = MakeColor(0, 49, 83);
 int colortav = MakeColor(139, 69, 19);
 int coloretav2 = MakeColor(0, 100, 0);
@@ -18,10 +17,10 @@ void run() {
         Clear(colore);
         UseAntiAliasing();
    
-        DrawCircle(960, 0, 1030, colortav, colortav);
-        DrawCircle(960, 0, 880, coloretav2, coloretav2);
+        DrawCircle(960, 0, 1030, colortav, Black);
+        DrawCircle(960, 0, 880, coloretav2, Black);
         carta(IMM2D_WIDTH/2-180/2, IMM2D_HEIGHT/2+265/10);
-        DrawRectangle(0, 1080-150, 100, 100, Black, White);
+        DrawRectangle(0, 1080-0, 100, 100, Black, White);
         
        
 
@@ -32,10 +31,19 @@ void run() {
 void carta(int x, int y)
 {
     srand(time(NULL));
-    int num = rand() % 9 + 2;
-    string u = to_string(num);
+    int num = RandomInt(2,9);
+    string lettera = "AJQK";
+    string u;
+    if (RandomInt(1,2)==1)
+    {
+        u = to_string(num);
+    }
+    else
+    {
+        u = lettera[RandomInt(0, 3)];
+    }
     DrawRectangle(x, y, 180, 265, White, Red);
-    DrawString(x,y, u.c_str(),"elvetica",50,Red,false);
-    DrawString(x + 123, y + 195,u.c_str(), "elvetica", 50, Red, false);
+    DrawString(x,y, u.c_str(),"elvetica",30,Red,false);
+    DrawString(x + 147, y + 224,u.c_str(), "elvetica", 30, Red, false);
 
 }
